d8472b93c2563e7c068595ce37f704362b3e1770
htg <h.atef@nu.edu.eg>
2024-12-08 03:51:27 +0200

    add and configure circuitBreaker in the API-Gateway

--- api-gateway/src/main/java/com/moghoneim/apigateway/routes/Routes.java
+++ api-gateway/src/main/java/com/moghoneim/apigateway/routes/Routes.java
@@ -1,13 +1,17 @@
 package com.moghoneim.apigateway.routes;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 @Configuration
 public class Routes {
@@ -15,7 +19,10 @@ public class Routes {
     RouterFunction<ServerResponse> usersServiceRoute() {
         return GatewayRouterFunctions.route("users_service")
                 .route(RequestPredicates.path("/api/v1/users/**"),
-                        HandlerFunctions.http("http://localhost:8082/")).build();
+                        HandlerFunctions.http("http://localhost:8082/"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("usersServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                )).build();
     }
 
     @Bean
@@ -23,6 +30,9 @@ public class Routes {
         return GatewayRouterFunctions.route("user_service_swagger")
                 .route(RequestPredicates.path("/aggregate/user-service/v3/api-docs"),
                         HandlerFunctions.http("http://localhost:8082"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("usersServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                ))
                 .filter(FilterFunctions.setPath("/api-docs"))
                 .build();
     }
@@ -31,7 +41,10 @@ public class Routes {
     RouterFunction<ServerResponse> guestsServiceRoute() {
         return GatewayRouterFunctions.route("guests_service")
                 .route(RequestPredicates.path("/api/v1/guests/**"),
-                        HandlerFunctions.http("http://localhost:8081/")).build();
+                        HandlerFunctions.http("http://localhost:8081/"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("guestsServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                )).build();
     }
 
     @Bean
@@ -39,6 +52,9 @@ public class Routes {
         return GatewayRouterFunctions.route("guests_service_swagger")
                 .route(RequestPredicates.path("/aggregate/guests-service/v3/api-docs"),
                         HandlerFunctions.http("http://localhost:8081"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("guestsServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                ))
                 .filter(FilterFunctions.setPath("/api-docs"))
                 .build();
     }
@@ -47,7 +63,10 @@ public class Routes {
     RouterFunction<ServerResponse> rsvpServiceRoute() {
         return GatewayRouterFunctions.route("rsvp_service")
                 .route(RequestPredicates.path("/api/v1/rsvp/**"),
-                        HandlerFunctions.http("http://localhost:8083/")).build();
+                        HandlerFunctions.http("http://localhost:8083/"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("rsvpServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                )).build();
     }
 
     @Bean
@@ -55,6 +74,9 @@ public class Routes {
         return GatewayRouterFunctions.route("rsvp_service_swagger")
                 .route(RequestPredicates.path("/aggregate/rsvp-service/v3/api-docs"),
                         HandlerFunctions.http("http://localhost:8083"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("rsvpServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                ))
                 .filter(FilterFunctions.setPath("/api-docs"))
                 .build();
     }
@@ -63,7 +85,11 @@ public class Routes {
     RouterFunction<ServerResponse> vendorsServiceRoute() {
         return GatewayRouterFunctions.route("vendors_service")
                 .route(RequestPredicates.path("/api/v1/vendors/**"),
-                        HandlerFunctions.http("http://localhost:8085/")).build();
+                        HandlerFunctions.http("http://localhost:8085/"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("vendorsServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                ))
+                .build();
 
 
     }
@@ -73,6 +99,9 @@ public class Routes {
         return GatewayRouterFunctions.route("vendors_service_swagger")
                 .route(RequestPredicates.path("/aggregate/vendors-service/v3/api-docs"),
                         HandlerFunctions.http("http://localhost:8085"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("vendorsServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                ))
                 .filter(FilterFunctions.setPath("/api-docs"))
                 .build();
     }
@@ -83,6 +112,9 @@ public class Routes {
         return GatewayRouterFunctions.route("discovery_server")
                 .route(RequestPredicates.path("/eureka/web"),
                         HandlerFunctions.http("http://localhost:8761/"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("discoveryServerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                ))
                 .filter(FilterFunctions.setPath("/"))
                 .build();
     }
@@ -92,6 +124,20 @@ public class Routes {
         return GatewayRouterFunctions.route("discovery_server_static")
                 .route(RequestPredicates.path("/eureka/**"),
                         HandlerFunctions.http("http://localhost:8761/"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("discoveryServerStaticCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                ))
+                .build();
+    }
+
+
+    @Bean
+    RouterFunction<ServerResponse> fallbackRoute() {
+        return GatewayRouterFunctions.route("fallbackRoute")
+                .GET("fallbackRoute",
+                        request -> ServerResponse
+                                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("Unfortunately, service is unavailable at the moment, please try again later."))
                 .build();
     }
 
