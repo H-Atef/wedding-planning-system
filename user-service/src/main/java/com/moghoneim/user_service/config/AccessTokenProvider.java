ba6dd581f5f9a1e4ce06bf09cabe42917ce7caac
mo-ghoniem <moghoniem9@gmail.com>
2025-04-26 05:36:06 +0300

    fix: changed Bearer to JwtAuthToken in AccessTokenProvider class

--- user-service/src/main/java/com/moghoneim/user_service/config/AccessTokenProvider.java
+++ user-service/src/main/java/com/moghoneim/user_service/config/AccessTokenProvider.java
@@ -1,7 +1,7 @@
 package com.moghoneim.user_service.config;
 
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,12 +10,13 @@ public class AccessTokenProvider {
     public String getAccessToken() {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication instanceof BearerTokenAuthentication bearerToken) {
-            System.out.println(bearerToken.getToken().getTokenValue());
-            return bearerToken.getToken().getTokenValue();
+        if (authentication instanceof JwtAuthenticationToken jwtAuth) {
+            return jwtAuth.getToken().getTokenValue();
         }
 
+        System.out.println("⚠️ Unexpected authentication type: " + (authentication != null ? authentication.getClass() : "null"));
         return null;
     }
 }
+
 
